37cd0824590dbe20e0733fc0eb2db9da58df965e
Benjamin Martin <bamartin@email.arizona.edu>
2015-09-08 11:32:46 -0700

    example.py is now an example
    I had accidentally the wrong file

--- under-development/example.py
+++ under-development/example.py
@@ -1,57 +1,23 @@
 #!/usr/bin/env python2
 
-import a3
+from __future__ import absolute_import
 
 import os
 import logging
 import h5py
-import curses
 
-logging.basicConfig(filename='a3.log',level=logging.INFO)
+import a3
 
-class A3CL():
-    def __init__(self):
-        self.win = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        self.win.keypad(1)
-        self.menu = Menu()
-
-    def __enter__(self):
-        return self
-
-    def __call__(self):
-        self.menu.refresh(self.x)
-        while True:
-            c = self.win.getch()
-            if c == ord('q'):
-                break
-            
-    @property
-    def y(self):
-        return self.win.getmaxyx()[0]
-    
-    @property
-    def x(self):
-        return self.win.getmaxyx()[1]
-
-    def __exit__(self, a,b,c):
-        curses.nocbreak()
-        self.win.keypad(0)
-        curses.echo()
-        curses.endwin()
-
-class Menu():
-    def __init__(self):
-        self.win = curses.newpad(1,1024)
-        self.win.addstr(0,0,"^N(ew)  ^O(pen)") 
-        self.win.chgat(0,0,-1,curses.A_REVERSE)
-        self.x = 0
-        
-    def refresh(self,width):
-        self.win.refresh(2,2, 0,0, 1,20)
-
-
-with A3CL() as session:
-    session()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    roi = a3.ROI(140.,320.,250.,580.)
+    if not os.path.isfile('test.hdf5'):
+        a3.get_from_files('test_data','test.hdf5',roi,buff=10,scale=.1)
+    if not os.path.isfile('train.hdf5'):
+        with h5py.File('test.hdf5','r') as h:
+            a3.get_from_files('apil-data/Interspeech2014_exp/',
+                'train.hdf5',roi,scale=.1,blacklist=set(h['name']))
+    a = a3.Autotracer('train.hdf5','test.hdf5',roi)
+    a.train()
+    with h5py.File('test.hdf5','r') as h:
+        a.trace(a.X_valid,'traces.json',h['name'],'autotest','042')
